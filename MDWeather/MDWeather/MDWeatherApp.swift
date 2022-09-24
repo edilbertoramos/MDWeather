3cd6cd93161251a7eef001ca8bb404d907815330
Edilberto Ramos <edilberto.ramos@anestech.com.br>
2022-09-24 14:20:07 -0300

    [feat] Create Xcode project

---
+++ MDWeather/MDWeather/MDWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDWeatherApp.swift
+//  MDWeather
+//
+//  Created by Edilberto Ramos on 24/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MDWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
